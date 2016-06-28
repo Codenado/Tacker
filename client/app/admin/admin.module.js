4711723c18739ca1e7a2b8ec3f95fe71b0e44e83
Jeffrey Leonard <jeffrey9279@gmail.com>
2016-06-28 13:21:52 -0700

    first

---
+++ client/app/admin/admin.module.js
@@ -0,0 +1,3 @@
+'use strict';
+
+angular.module('tackiApp.admin', ['tackiApp.auth', 'ui.router']);
---
+++ client/components/auth/auth.module.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('tackiApp.auth', ['tackiApp.constants', 'tackiApp.util', 'ngCookies', 'ui.router'])
+  .config(function($httpProvider) {
+    $httpProvider.interceptors.push('authInterceptor');
+  });
